e0b60f768d08d2b65ba8b2c1dfa27c2346c121e4
Anton Nayshtut <anton@daynix.com>
2013-10-28 12:47:49 +0000

    VirtHCK: Fixes #6: use WTTSTUDIO environment variable while running HCK Studio

--- VirtHCK/guest_tools/run_hck_studio.bat
+++ VirtHCK/guest_tools/run_hck_studio.bat
@@ -14,8 +14,8 @@ echo Disable external network...
 wmic path win32_networkadapter where name="Intel(R) PRO/1000 MT Network Connection" call disable
 
 echo Run HCK studio...
-cd "C:\Program Files (x86)\Windows Kits\8.0\Hardware Certification Kit\Studio\"
-"C:\Program Files (x86)\Windows Kits\8.0\Hardware Certification Kit\Studio\hckstudio.exe"
+cd "%WTTSTDIO%"
+"%WTTSTDIO%\hckstudio.exe"
 
 echo Enable network...
 wmic path win32_networkadapter where name="Intel(R) PRO/1000 MT Network Connection" call enable
