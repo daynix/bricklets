204700685508f8f6f1f5d2ccc928f3c7e6437552
Dmitry Fleytman <dmitry@daynix.com>
2015-02-19 13:45:24 +0200

    VirtHCK: Increase amount of RAM allocated for studio machine
    Updating HCK filters fails due to insufficient memory.
    
    Signed-off-by: Dmitry Fleytman <dmitry@daynix.com>

--- VirtHCK/run_hck_studio.sh
+++ VirtHCK/run_hck_studio.sh
@@ -30,7 +30,7 @@ ${QEMU_BIN} \
     -drive file=${STUDIO_IMAGE},if=ide${DRIVE_CACHE_OPTION} \
     ${WORLD_NET_DEVICE} \
     ${CTRL_NET_DEVICE} \
-    -m 512M -smp 1 -enable-kvm -cpu qemu64,+x2apic -usbdevice tablet \
+    -m 768M -smp 1 -enable-kvm -cpu qemu64,+x2apic -usbdevice tablet \
     -uuid 9999127c-8795-4e67-95da-8dd0a8891cd1 \
     -name HCK-Studio_${UNIQUE_ID}_`hostname`${_TITLE_POSTFIX} \
     -rtc base=localtime \
