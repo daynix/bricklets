272bf4d42aac188403047225ab379d4f935ee955
Leonid Bloch <leonid@daynix.com>
2015-04-14 17:14:41 +0300

    VirtHCK: RemoteHCK: Some usability and future-proofing
    * Ordered the parameters in RemoteHCK.sh by expected frequency of modification. Commented on that in the code.
    * Swithched the sed separator from "/" to "|", in case that some paths will need to be replaced in the future.
    * Set the global ("g") option in sed, in case that multiple replacements per line will be needed. Currently there are only single replacements per line, but adding that option will not hurt, and may save time in the future.
    
    Signed-off-by: Leonid Bloch <leonid@daynix.com>

--- VirtHCK/guest_tools/RemoteHCK/RemoteHCK.sh
+++ VirtHCK/guest_tools/RemoteHCK/RemoteHCK.sh
@@ -2,18 +2,24 @@
 
 # Automate setup for VirtHCK
 
-controllerIP='10.0.1.212'
-cl1Name='CL1-2012R2X64'
-cl2Name='CL2-2012R2X64'
-winPasswd='PUT_YOUR_PASSWORD_HERE'
-testDevice='PCI\VEN_XXXX&DEV_XXXX&SUBSYS_XXXXXXXX'  #Format: PCI\VEN_XXXX&DEV_XXXX&SUBSYS_XXXXXXXX
-HCKVersion='8.1'  # Important! Change if using a different version!
-projectName='TEST-PROJ'
-
 SCRIPTS_DIR=`dirname $0`
 
+#Frequwntly changed
+testDevice='PCI\VEN_XXXX&DEV_XXXX&SUBSYS_XXXXXXXX'  #Format: PCI\VEN_XXXX&DEV_XXXX&SUBSYS_XXXXXXXX
+projectName='TEST-PROJ'
+cl1Name='CL1-2012R2X64'
+cl2Name='CL2-2012R2X64'
+
+#Occasionally changed
+controllerIP='10.0.1.212'
+winPasswd='PUT_YOUR_PASSWORD_HERE'
+HCKVersion='8.1'  # Important! Change if using a different version!
+
+#Rarely changed
 SMBShareDir="${SCRIPTS_DIR}/../../SMB_SHARE"
 SHARE_ON_HOST_ADDR='192.168.101.1'
+
+#Don't change, unless REALLY needed!
 autoHCKFile="${SCRIPTS_DIR}/../AutoHCK/AutoHCK.ps1"
 sysSetupFile="${SCRIPTS_DIR}/SYS_SETUP.bat"
 sysSetupAuxFile="${SCRIPTS_DIR}/SYS_SETUP_AUX.ps1"
@@ -40,26 +46,27 @@ then
     echo "domain=WORKGROUP" >> "$credsFile"
     # Change names in scripts and send changed scripts to shared folder.
     echo "Changing settings in setup scripts and copying them to $SMBShareDir ..."
-    sed "s/CL1-REPLACE/$cl1Name/" "$sysSetupAuxFile" > "$SMBShareDir/${sysSetupAuxFile##*/}"
-    sed -i "s/CL2-REPLACE/$cl2Name/" "$SMBShareDir/${sysSetupAuxFile##*/}"
     #
-    sed "s/CL1-REPLACE/$cl1Name/" "$autoHCKFile" > "$SMBShareDir/${autoHCKFile##*/}"
-    sed -i "s/CL2-REPLACE/$cl2Name/" "$SMBShareDir/${autoHCKFile##*/}"
-    sed -i "s/DEVICE-REPLACE/$(printf "%q" "$testDevice")/" "$SMBShareDir/${autoHCKFile##*/}"
-    sed -i "s/TEST-REPLACE/$projectName/" "$SMBShareDir/${autoHCKFile##*/}"
-    sed -i "s/REPLACE-SMB-ADDRESS/$SHARE_ON_HOST_ADDR/g" "$SMBShareDir/${sysSetupAuxFile##*/}"
+    sed "s|CL1-REPLACE|$cl1Name|g" "$sysSetupAuxFile" > "$SMBShareDir/${sysSetupAuxFile##*/}"
+    sed -i "s|CL2-REPLACE|$cl2Name|g" "$SMBShareDir/${sysSetupAuxFile##*/}"
+    sed -i "s|REPLACE-SMB-ADDRESS|$SHARE_ON_HOST_ADDR|g" "$SMBShareDir/${sysSetupAuxFile##*/}"
     sed -i "s|REPLACE-CONTROLLER-INST-FILE|${ControllerInstFile##*/}|g" "$SMBShareDir/${sysSetupAuxFile##*/}"
     sed -i "s|REPLACE-CLIENT-INST-FILE|${ClientInstFile##*/}|g" "$SMBShareDir/${sysSetupAuxFile##*/}"
     #
-    sed "s/PASSWORD-REPLACE/$winPasswd/" "$sysSetupFile" > "$SMBShareDir/${sysSetupFile##*/}"
-    sed -i "s/BGI-REPLACE/${bgiFile##*/}/" "$SMBShareDir/${sysSetupFile##*/}"
-    sed -i "s/REPLACE-SMB-ADDRESS/$SHARE_ON_HOST_ADDR/g" "$SMBShareDir/${sysSetupFile##*/}"
+    sed "s|CL1-REPLACE|$cl1Name|g" "$autoHCKFile" > "$SMBShareDir/${autoHCKFile##*/}"
+    sed -i "s|CL2-REPLACE|$cl2Name|g" "$SMBShareDir/${autoHCKFile##*/}"
+    sed -i "s|DEVICE-REPLACE|$(printf "%q" "$testDevice")|g" "$SMBShareDir/${autoHCKFile##*/}"
+    sed -i "s|TEST-REPLACE|$projectName|g" "$SMBShareDir/${autoHCKFile##*/}"
+    #
+    sed "s|PASSWORD-REPLACE|$winPasswd|g" "$sysSetupFile" > "$SMBShareDir/${sysSetupFile##*/}"
+    sed -i "s|BGI-REPLACE|${bgiFile##*/}|g" "$SMBShareDir/${sysSetupFile##*/}"
+    sed -i "s|REPLACE-SMB-ADDRESS|$SHARE_ON_HOST_ADDR|g" "$SMBShareDir/${sysSetupFile##*/}"
     sed -i "s|REPLACE-SETUP-AUX|${sysSetupAuxFile##*/}|g" "$SMBShareDir/${sysSetupFile##*/}"
     #
-    sed "s/REPLACE-HCK-VERSION/$HCKVersion/" "$ControllerInstFile" > "$SMBShareDir/${ControllerInstFile##*/}"
-    sed -i "s/REPLACE-SMB-ADDRESS/$SHARE_ON_HOST_ADDR/g" "$SMBShareDir/${ControllerInstFile##*/}"
+    sed "s|REPLACE-HCK-VERSION|$HCKVersion|g" "$ControllerInstFile" > "$SMBShareDir/${ControllerInstFile##*/}"
+    sed -i "s|REPLACE-SMB-ADDRESS|$SHARE_ON_HOST_ADDR|g" "$SMBShareDir/${ControllerInstFile##*/}"
     #
-    sed "s/REPLACE-SMB-ADDRESS/$SHARE_ON_HOST_ADDR/g" "$ClientInstFile" > "$SMBShareDir/${ClientInstFile##*/}"
+    sed "s|REPLACE-SMB-ADDRESS|$SHARE_ON_HOST_ADDR|g" "$ClientInstFile" > "$SMBShareDir/${ClientInstFile##*/}"
     #
     cp "$bgiFile" "$SMBShareDir/${bgiFile##*/}"
     # Create batch file to run AutoHCK
